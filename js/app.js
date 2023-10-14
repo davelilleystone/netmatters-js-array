6654cff58083c827ba099b76256b64d7002d3ecd
Dave Lilleystone <davelilleystone@gmail.com>
2023-10-14 13:17:52 +0100

    add comments to app.js, refactor functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://api.unsplash.com/photos/random";
+const apiUrl = "https://api.unsplash.com/photos/random/?count=1&orientation=landscape";
 const baseUrl = "https://unsplash.com/photos";
 let currentImageData = {};
 
@@ -13,52 +13,94 @@ const emailSelect = document.querySelector(".email-select");
 
 emailForm.noValidate = true;
 
-getEmailFromSelect = () => {
-  return emailSelect.value;
+/********************/
+/* Handler functions */
+/********************/
+
+// on page load handler
+
+onPageLoadHandler = () => {
+  // check if account array exists in local storage - if not, create it
+  if (!localStorage.getItem("accounts")) {
+    localStorage.setItem("accounts", JSON.stringify([]));
+  }
+  // if no accounts exist,  no need to load select list
+  if (getAccountsFromLocalStorage().length === 0) {
+    return;
+  } else {
+    const emailAddresses = getEmailAddresses();
+    removeDefaultOption();
+    emailAddresses.sort().forEach((email) => {
+      addEmailToSelect(email);
+    });
+  }
+  // fire change event on select so we can show saved images for the first email in the list
+  fireSelectChangeEvent(emailSelect);
+  //   displayImage(apiUrl, photoContainer);
 };
 
-saveImageToAccount = (account, image) => {
-  account.images.push(image);
-  const accounts = getAccountsFromLocalStorage();
-  for (let i = 0; i < accounts.length; i++) {
-    if (accounts[i].email === account.email) {
-      accounts[i] = account;
-    }
-  }
-  localStorage.setItem("accounts", JSON.stringify(accounts));
-};
+// save image to account and add to saved images container on page
 
 onSaveImageHandler = (e) => {
+  console.log("save image handler");
   if (!checkEmailOnSave(emailSelect)) {
     return console.log("No email selected");
   }
   const account = getAccountFromLocalStorage(getEmailFromSelect());
   const image = getCurrentImageData();
 
-  // check if image is already saved
-  if (account.images.some((savedImage) => savedImage.id === image.id)) {
+  if (checkImageIsSaved(image, account)) {
     return console.log("Image already saved");
   }
   saveImageToAccount(account, image);
 };
 
+/********************/
+/* Helper functions */
+/********************/
+
+// check if main image exists on page
 const checkMainImageExists = () => {
   return photoContainer.querySelector(".main-image") ? true : false;
 };
 
+// get the current image data - updates on each search
 const getCurrentImageData = () => {
   return currentImageData;
 };
 
+// store the current image data
 const setCurrentImageData = (imageData) => {
   currentImageData = imageData;
 };
 
-const getEmailAddresses = () => {
-  const accounts = getAccountsFromLocalStorage();
-  return accounts.map((account) => account.email);
+/********************/
+/* Select functions */
+/********************/
+
+/* fire change event on select - example use is when emails are 
+added to select list on page load, this event is fired so that 
+we can call the handler that loads a users stored images */
+
+const fireSelectChangeEvent = (select) => {
+  select.dispatchEvent(new Event("change"));
 };
 
+// remove default option from select list - default option is used to show placeholder text
+const removeDefaultOption = () => {
+  const defaultOption = emailSelect.querySelector("option[value='default-option']");
+  if (defaultOption) {
+    emailSelect.removeChild(defaultOption);
+  }
+};
+
+// check if default option exists in select list
+const checkDefaultOptionExists = () => {
+  const defaultOption = emailSelect.querySelector("option[value='default-option']");
+  return defaultOption ? true : false;
+};
+
+// set selected option in select list
 const setSelected = (select, value) => {
   for (let i = 0; i < select.options.length; i++) {
     if (select.options[i].value === value) {
@@ -68,24 +110,22 @@ const setSelected = (select, value) => {
   }
 };
 
-const fireSelectChangeEvent = (select) => {
-  select.dispatchEvent(new Event("change"));
+// get selected email from select list
+getEmailFromSelect = () => {
+  return emailSelect.value;
 };
 
-const removeDefaultOption = () => {
-  const defaultOption = emailSelect.querySelector("option[value='default-option']");
-  if (defaultOption) {
-    emailSelect.removeChild(defaultOption);
-  }
+/*******************/
+/* Email functions */
+/*******************/
+
+// get all email addresses from local storage
+const getEmailAddresses = () => {
+  const accounts = getAccountsFromLocalStorage();
+  return accounts.map((account) => account.email);
 };
 
-const checkDefaultOptionExists = () => {
-  const defaultOption = emailSelect.querySelector("option[value='default-option']");
-  return defaultOption ? true : false;
-};
-
-// add email to select list
-
+// check if any email addresses exist in select list
 const checkEmailOnSave = (email) => {
   if (checkDefaultOptionExists()) {
     return false;
@@ -93,16 +133,19 @@ const checkEmailOnSave = (email) => {
   return getEmailFromSelect();
 };
 
+// check if email syntax is valid
 const checkEmailIsValid = (email) => {
   const emailRegex = new RegExp(/^[a-zA-Z0-9._\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,6}$/);
   return emailRegex.test(email);
 };
 
+// check if submitted email already exists
 const checkEmailExists = (email) => {
   const accounts = getAccountsFromLocalStorage();
   return accounts.some((account) => account.email === email.toLowerCase());
 };
 
+// add email to select list
 const addEmailToSelect = (email) => {
   const option = document.createElement("option");
   option.value = email;
@@ -110,36 +153,30 @@ const addEmailToSelect = (email) => {
   emailSelect.appendChild(option);
 };
 
-const createAccount = (email) => {
-  return { email: email.toLowerCase(), images: [] };
-};
-
-const addAccountToLocalStorage = (account) => {
-  const accounts = getAccountsFromLocalStorage();
-  accounts.push(account);
-  localStorage.setItem("accounts", JSON.stringify(accounts));
-};
-
-const getAccountsFromLocalStorage = () => {
-  const accounts = localStorage.getItem("accounts") ? JSON.parse(localStorage.getItem("accounts")) : [];
-  return accounts;
-};
-
-const getAccountFromLocalStorage = (email) => {
-  const accounts = getAccountsFromLocalStorage();
-  return accounts.find((account) => account.email === email);
-};
-
-const addEmail = (email) => {
-  // check if email is valid
+// check if email syntax is valid and does not already exist
+const validateEmail = (email) => {
+  // check if email syntax is valid
   if (!checkEmailIsValid(email)) {
-    return console.log("Email is not valid");
+    console.log("Email is not valid");
+    return false;
   }
 
   // check if email exists in local storage
   if (checkEmailExists(email)) {
-    return console.log("Email already exists");
+    console.log("Email already exists");
+    return false;
   }
+
+  return true;
+};
+
+const addEmail = (email) => {
+  // validate email
+  if (!validateEmail(email)) {
+    console.log("Email is not valid");
+    return;
+  }
+
   // email is ok, create account
   const account = createAccount(email);
 
@@ -158,6 +195,41 @@ const addEmail = (email) => {
   setSelected(emailSelect, email);
   // fire change event on select
   fireSelectChangeEvent(emailSelect);
+};
+
+/*********************/
+/* Account Functions */
+/*********************/
+
+// add account to local storage
+const addAccountToLocalStorage = (account) => {
+  const accounts = getAccountsFromLocalStorage();
+  accounts.push(account);
+  localStorage.setItem("accounts", JSON.stringify(accounts));
+};
+
+// check if image has already been saved to account
+const checkImageIsSaved = (image, account) => {
+  if (account.images.some((savedImage) => savedImage.id === image.id)) {
+    return true;
+  }
+  return false;
+};
+
+const createAccount = (email) => {
+  return { email: email.toLowerCase(), images: [] };
+};
+
+// get single account from local storage
+const getAccountFromLocalStorage = (email) => {
+  const accounts = getAccountsFromLocalStorage();
+  return accounts.find((account) => account.email === email);
+};
+
+// get all accounts from local storage
+const getAccountsFromLocalStorage = () => {
+  const accounts = localStorage.getItem("accounts") ? JSON.parse(localStorage.getItem("accounts")) : [];
+  return accounts;
 };
 
 /*
@@ -205,8 +277,9 @@ depending on if the user has entered a search term */
 
 const displayImage = async (url, parent) => {
   try {
-    const imageData = await getImageData(url);
+    const [imageData] = await getImageData(url);
     const image = createImage(imageData);
+    console.log(imageData);
     addImageToDom(image, parent);
     setCurrentImageData(imageData);
   } catch (err) {
@@ -228,7 +301,7 @@ searchForm.addEventListener("submit", (e) => {
     photoContainer.removeChild(photoContainer.firstChild);
   }
   const searchTerm = searchInput.value.trim();
-  const url = searchTerm != "" ? `${apiUrl}?query=${encodeURIComponent(searchTerm)}` : apiUrl;
+  const url = searchTerm != "" ? `${apiUrl}&query=${encodeURIComponent(searchTerm)}` : apiUrl;
   displayImage(url, photoContainer);
   //   searchInput.value = "";
 });
@@ -251,21 +324,17 @@ emailSelect.addEventListener("change", (e) => {
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  // check if account array exists in local storage - if not, create it
-  if (!localStorage.getItem("accounts")) {
-    localStorage.setItem("accounts", JSON.stringify([]));
-  }
-  // if no accounts exist,  no need to load select list
-  if (getAccountsFromLocalStorage().length === 0) {
-    return;
-  } else {
-    const emailAddresses = getEmailAddresses();
-    removeDefaultOption();
-    emailAddresses.sort().forEach((email) => {
-      addEmailToSelect(email);
-    });
-  }
-  // fire change event on select so we can show saved images for the first email in the list
-  fireSelectChangeEvent(emailSelect);
-  //   displayImage(apiUrl, photoContainer);
+  onPageLoadHandler();
 });
+
+const saveImageToAccount = (account, image) => {
+  account.images.push(image);
+  updateAccountInLocalStorage(account);
+};
+
+const updateAccountInLocalStorage = (account) => {
+  const accounts = getAccountsFromLocalStorage();
+  const index = accounts.findIndex((acc) => acc.email === account.email);
+  accounts[index] = account;
+  localStorage.setItem("accounts", JSON.stringify(accounts));
+};
